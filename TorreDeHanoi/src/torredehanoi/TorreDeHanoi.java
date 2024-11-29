be028da234aa1855a4aa630f1a4c475d6897c505
JuanPabloMarcano <jmarcano.1701@unimar.edu.ve>
2024-11-29 14:30:43 -0400

    Pilas Origen,Destino y Auxiliar

--- TorreDeHanoi/src/torredehanoi/TorreDeHanoi.java
+++ TorreDeHanoi/src/torredehanoi/TorreDeHanoi.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package torredehanoi;
+//Edwing Rodriguez y Juan Marcano
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class TorreDeHanoi {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        //Objetos de Tipo Pila que son los que almacenaran los discos
+        Pila origen = new Pila();
+        Pila destino = new Pila();
+        Pila auxiliar = new Pila();
+        
+        TorreDeHanoi torre = new TorreDeHanoi();
+        torre.Elegir(opcion);
     }
     
+    void Elegir(int opcion){
+        Scanner op = new Scanner(System.in);
+        System.out.println("Ingrese a cual Pila Quiere ir \n"
+                + "1. Origen \n"
+                + "2. Destino \n"
+                + "3. Auxiliar \n");
+        opcion = op.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println("Esta en la pila origen");
+                break;
+            case 2:
+                System.out.println("Esta en la pila destino");
+                break;
+            case 3:
+                System.out.println("esta en la pila auxiliar");
+            default:
+                throw new AssertionError();
+        }
+    }
 }
