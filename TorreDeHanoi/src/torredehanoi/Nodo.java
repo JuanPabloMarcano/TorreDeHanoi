13f0ece5dba5924c15f641cf6cdb1917024b5951
JuanPabloMarcano <jmarcano.1701@unimar.edu.ve>
2024-12-02 17:14:28 -0400

    Modificaciones a la Clase Nodo y Pila

--- TorreDeHanoi/src/torredehanoi/Nodo.java
+++ TorreDeHanoi/src/torredehanoi/Nodo.java
@@ -2,8 +2,9 @@ package torredehanoi;
 
 public class Nodo {
     int informacion;
-    Nodo siguiente;
-    String dato;
+    private Nodo siguiente;
+    private Nodo down; //La palabra favorita de la clase
+    private String dato;
     
     public Nodo(int valor){
         informacion = valor;
@@ -28,5 +29,29 @@ public class Nodo {
 
     public void setInformacion(int informacion) {
         this.informacion = informacion;
+    }
+
+    public Nodo getDown() {
+        return down;
+    }
+
+    public void setDown(Nodo down) {
+        this.down = down;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
     }
 }
--- TorreDeHanoi/src/torredehanoi/Pila.java
+++ TorreDeHanoi/src/torredehanoi/Pila.java
@@ -1,66 +1,36 @@
 package torredehanoi;
 
 public class Pila {
-    Nodo top;
-    Pila aux;
-    String dato;
-    
-    boolean isEmpty(){ // comprobar si existen elementos en la pila o no
-        if (top == null){ // si top esta asignado es porque existe un elemento al menos
-            return true;
-        }else{
-            return false;
-        }
+    private Nodo top;
+    private int valorNodo = 0;
+
+    public int getvalorNodo() {
+        return valorNodo;
     }
-    void push(Nodo elemento){ // insertar un elemento nuevo al tope de la pila
-        if (this.isEmpty()){ // Si no existe nada almacenado en la pila asigna a top el elemento entrante
-            top = elemento;
-        }else{ // si ya existen elementos agregados se adjuntan los elementos actuales de la pila al
-            elemento.siguiente = this.top; // Nodo que esta entrando, y este elemento pasa a ser TOP
-            this.top = elemento;
-        }
-    }
-    Nodo pop(){
-        if (!this.isEmpty()){
-            Nodo temp = new Nodo();
-            temp.dato = this.top.dato; // asigno los datos de top al Nodo temporal
-            this.top = this.top.siguiente;
-            return temp;
-        }else{
-            System.out.println("Pila Vacía");
-            return null;
-        }
-    }
-    void destroy(String valor){
-        aux = new Pila();
-        while (this.top!=null){
-            if (this.top.dato.equals(valor)) {
-                System.out.println("Eliminado " + this.pop().dato);
-                break;
-            }else{
-            aux.push(this.pop()); // paso todos los elementos existentes de "this" a "aux"
-            }
-        }
-        while (aux.top!=null){
-            this.push(aux.pop());
-        }
-    }
-    void edit(String valor , String nuevo){
-        aux = new Pila();
-        while (this.top!=null){
-            if (this.top.dato.equals(valor)) {
-                System.out.println("Modificado \"" + this.top.dato +"\" por el valor \"" + nuevo +"\"");
-                top.dato = nuevo;
-            }else{
-            aux.push(this.pop()); // paso todos los elementos existentes de "this" a "aux"
-            }
-        }
-        while (aux.top!=null){
-            this.push(aux.pop());
-        }
+
+    public Nodo getTop() {
+        return top;
     }
     
-    public int Peek(){
-        return top.getInformacion();
+    public void Push (Nodo N){
+        valorNodo++;
+        if (top==null){
+            top=N;
+        }else{
+            N.setDown(top);
+            top.setSiguiente(N);
+            top = N;
+            
+        }
+    }  
+    public void Pop(){
+        if(valorNodo > 0){
+            valorNodo--;
+            top = top.getDown();
+        }
     }
+    public String Peek (){
+        return top.getDato();
+    }
+
 }
