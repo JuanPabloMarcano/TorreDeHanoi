5aff81488f10a7288714ed30bbdb3535a4e5f156
JuanPabloMarcano <jmarcano.1701@unimar.edu.ve>
2024-12-03 11:28:58 -0400

    Metodo Mover Discos

--- TorreDeHanoi/src/torredehanoi/TorreDeHanoi.java
+++ TorreDeHanoi/src/torredehanoi/TorreDeHanoi.java
@@ -7,29 +7,27 @@ public class TorreDeHanoi {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner op = new Scanner(System.in);
+        int opcion = 0;
         
         //Objetos de Tipo Pila que son los que almacenaran los discos
         Pila origen = new Pila();
         Pila destino = new Pila();
         Pila auxiliar = new Pila();
         
+        // Objeto Tipo Nodo
+        Nodo N = new Nodo();
+        
         //Obejeto de Tipo Lista Circular que contendra los Elementos de las torres
         ListaCircular torreOrigen = new ListaCircular();
         for (int i = 1; i < 9; i++) {
-            torreOrigen.agregar(i);
+            origen.Push(N(i));
         }
         
         System.out.println("Aqui se ven los valores que tiene la primera Torre");
         torreOrigen.mostrar();
         
-        //Aqui se elige la torre que quieres
-        TorreDeHanoi torre = new TorreDeHanoi();
-        torre.Elegir();
         
-    }
-    void Elegir(){
-        Scanner op = new Scanner(System.in);
-        int opcion = 0;
         System.out.println("Ingrese a cual Pila Quiere ir \n"
                 + "|1. Origen  | \n"
                 + "|2. Destino | \n"
@@ -38,6 +36,7 @@ public class TorreDeHanoi {
         switch (opcion) {
             case 1:
                 System.out.println("Esta en la pila origen");
+                origen.Peek();
                 break;
             case 2:
                 System.out.println("Esta en la pila destino");
@@ -45,7 +44,29 @@ public class TorreDeHanoi {
             case 3:
                 System.out.println("esta en la pila auxiliar");
             default:
-                throw new AssertionError();
+                System.out.println("NO");
+                break;
+        }
+        
+        //Aqui se elige la torre que quieres
+        TorreDeHanoi torre = new TorreDeHanoi();
+        torre.Elegir();
+        
+    }
+    void Elegir(){
+        System.out.println("");
+    }
+    
+    public static int moverDiscos(int n, Pila origen, Pila destino, Pila auxiliar) {
+        if (n == 1) {
+            System.out.println("Mover disco " + n + " de " + origen + " para " + destino);
+            return 1;
+        } else {
+            int movimentos = moverDiscos(n - 1, origen, auxiliar, destino);
+            System.out.println("Mover disco " + n + " de " + origen + " para " + destino);
+            movimentos++;
+            movimentos += moverDiscos(n - 1, auxiliar, destino, origen);
+            return movimentos;
         }
     }
 }
