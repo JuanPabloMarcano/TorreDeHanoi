c32cd4e7f8e7acc4f7ada19dcdd1ac1b835ac935
JuanPabloMarcano <jmarcano.1701@unimar.edu.ve>
2024-12-03 09:19:33 -0400

    Metodo Extraer agregado

--- TorreDeHanoi/src/torredehanoi/ListaCircular.java
+++ TorreDeHanoi/src/torredehanoi/ListaCircular.java
@@ -7,68 +7,37 @@ public class ListaCircular {
    void agregar(int pnum) {       
        if (L==null){
            L=new Nodo(pnum);
-           L.siguiente=L;
+           L.setSiguiente(L);
        }else{
            aux=L;
-           while(aux.siguiente!=L)
-               aux=aux.siguiente;
-           aux.siguiente=new Nodo(pnum, L);
+           while(aux.getSiguiente()!=L)
+               aux=aux.getSiguiente();
+           aux.setSiguiente(new Nodo(pnum, L));
        }
    }
     void mostrar (){
         aux=L;
         String dat="";
-        while (aux.siguiente!=L){
+        while (aux.getSiguiente()!=L){
             dat=dat+aux.getInformacion()+",";
-            aux=aux.siguiente;
+            aux=aux.getSiguiente();
         }
         dat=dat+aux.getInformacion()+", ";
         System.out.print("L-->"+dat);
-}
-    void eliminar(int valor){
-        aux=L;
-        Nodo aux2 = null;
-        while(aux.siguiente!=L || aux.getInformacion() != valor){
-            aux2=aux;
-            aux=aux.siguiente;
-        }
-        if (aux.getInformacion() == valor){
-            if (aux == L && aux.getInformacion() == valor) {
-                Nodo auxL=L;
-                L = L.siguiente;
-                while(aux.siguiente!=auxL)
-                    aux=aux.siguiente;
-                aux.siguiente = L;
-            }else{
-                aux2.siguiente = aux.siguiente;
-            }
-            System.out.println(valor + " Eliminado de la lista");
-        }else{
-            System.out.println("valor no encontrado en la lista");
-        }
     }
     
-    
-    void buscar(int valor){
-        aux=L;
-        if(aux!=null){
-            while (aux.siguiente!=L || aux.getInformacion() != valor)
-                aux= aux.siguiente;
-            if (aux.getInformacion() == valor){
-                System.out.println(valor + " Existe dentro de la lista");
-            }else{
-                System.out.println("valor no encontrado en la lista");
-            }
-        }
-    }
-    
-    void recorrer(Pila pila) {
-        if (L != null) {
-            Nodo actual = L;
-            do {
-                pila.push());
-                actual = actual.siguiente;
-            } while (actual != L);
-        }
+    public int extraer(){ 
+        if (L == null) return -1;// Lista vacía
+                int valor = Integer.parseInt(L.dato);
+                    if (L.siguiente == L) { // Un solo elemento en la lista 
+                L = null;   
+        }else{ 
+            Nodo temp = L; 
+                while (temp.siguiente != L){ 
+                        temp = temp.siguiente; 
+                    } 
+                temp.siguiente = L.siguiente; 
+            L = L.siguiente; 
+        } return valor;
     }
 }
